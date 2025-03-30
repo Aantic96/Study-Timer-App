c55244a8912731959762825458fcabd474bde91a
Antonela <gemory.mei@gmail.com>
2025-03-30 13:46:51 +0200

    Organized buttons, added reset button

--- app/src/main/java/org/example/StudyTimer.java
+++ app/src/main/java/org/example/StudyTimer.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
 import java.util.Timer;
@@ -11,6 +13,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 public class StudyTimer extends JPanel{
@@ -18,36 +21,53 @@ public class StudyTimer extends JPanel{
     private int seconds = 0;
     private Timer timer;
     private JLabel timeLabel;
-    private JButton startButton, pauseButton, exportButton;
+    private JButton startButton, pauseButton, exportButton, resetButton;
     private boolean isRunning = false;
     private int buttonSize = 50;
+    private JPanel controlsPanel;
+    private JPanel exportPanel;
     
     public StudyTimer() {
-        setBackground(Color.DARK_GRAY);
+        setLayout(new BorderLayout());
 
+        controlsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        exportPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         timeLabel = new JLabel("Time: 00:00:00");
         startButton = new JButton();
         pauseButton = new JButton();
+        resetButton = new JButton();
         exportButton = new JButton("Export");
 
         styleTextTimer(timeLabel);
+        timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         styleButton(startButton, "src/main/resources/icons/play.png", buttonSize);
         styleButton(pauseButton, "src/main/resources/icons/pause.png", buttonSize);
+        styleButton(resetButton, "src/main/resources/icons/reset.png", buttonSize);
 
-        add(timeLabel);
-        add(startButton);
-        add(pauseButton);
-        add(exportButton);
+        styleExportButton(exportButton);
+
+        setBackground(Color.DARK_GRAY);
+        controlsPanel.setBackground(Color.DARK_GRAY);
+        exportPanel.setBackground(Color.DARK_GRAY);
+
+        add(timeLabel, BorderLayout.NORTH);
+        add(controlsPanel, BorderLayout.CENTER);
+        add(exportPanel, BorderLayout.SOUTH);
+        controlsPanel.add(startButton);
+        controlsPanel.add(pauseButton);
+        controlsPanel.add(resetButton);
+        exportPanel.add(exportButton);
 
         startButton.addActionListener(e -> startTimer());
         pauseButton.addActionListener(e -> pauseTimer());
+        resetButton.addActionListener(e -> resetTimer());
         exportButton.addActionListener(e -> exportTime());
     }
 
     private void styleTextTimer(JLabel timeLabel) {
-        timeLabel.setForeground(Color.YELLOW);
-        timeLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        timeLabel.setForeground(Color.decode("#fece09"));
+        timeLabel.setFont(new Font("Arial", Font.BOLD, 30));
     }
 
     private void styleButton(JButton button, String iconPath, int buttonSize) {
@@ -58,11 +78,15 @@ public class StudyTimer extends JPanel{
     
         button.setPreferredSize(new Dimension(buttonSize, buttonSize));
     
+        button.setBackground(Color.decode("#242321"));
         button.setBorderPainted(false);
-        button.setFocusPainted(false);
         button.setContentAreaFilled(false);
     }
     
+    private void styleExportButton(JButton exportButton) {
+        exportButton.setForeground(Color.decode("#fece09"));
+        exportButton.setBackground(Color.decode("#242321"));
+    }
 
     private String formatTime() {
         int hours = seconds / 3600;
@@ -95,7 +119,19 @@ public class StudyTimer extends JPanel{
             timer = null;
             isRunning = false;
         }
+    }
 
+    private void resetTimer() {
+        if (timer != null) {
+            timer.cancel();
+            timer.purge();
+        }
+
+        isRunning = false;
+
+        seconds = 0;
+        timeLabel.setText("Time: 00:00:00");
+        timer = new Timer();
     }
 
     private void exportTime() {
